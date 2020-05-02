6eac8ec96028f2b76f76d3ea0ef6ca0cc522ad37
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-01 22:59:25 -0400

    Fix repository techs rendering

--- src/App.js
+++ src/App.js
@@ -42,12 +42,11 @@ export default function App() {
               <Text style={styles.repository}>{repository.title}</Text>
 
               <View style={styles.techsContainer}>
-                <Text style={styles.tech}>
-                  ReactJS
-                </Text>
-                <Text style={styles.tech}>
-                  Node.js
-                </Text>
+                {repository.techs.map(tech => (
+                  <Text key={tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+                ))}
               </View>
 
               <View style={styles.likesContainer}>
@@ -55,7 +54,7 @@ export default function App() {
                   style={styles.likeText}
                   testID={`repository-likes-${repository.id}`}
                 >
-                  {repository.likes} curtidas
+                  {repository.likes} curtida{repository.likes === 1 ? '' : 's'}
                 </Text>
               </View>
 
