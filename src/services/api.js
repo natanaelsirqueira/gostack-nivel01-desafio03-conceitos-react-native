dbe364dee498db32bffb5afa5fa7ed45549be442
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-01 22:02:40 -0400

    Added repositories functionality

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_HOST } from 'react-native-dotenv'
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: `http://${API_HOST}:3333`,
 });
 
 export default api;
