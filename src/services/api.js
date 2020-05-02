79bcd7c034d5f411bbd80dac5ff6dbe3a577d061
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-01 23:09:51 -0400

    Removed dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { API_HOST } from 'react-native-dotenv'
 
 const api = axios.create({
-  baseURL: `http://${API_HOST}:3333`,
+  baseURL: "http://localhost:3333",
 });
 
 export default api;
